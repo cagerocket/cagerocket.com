2e8cccaf6a725062ede05f8ead5285dab341c6ab
cometdesign <purchasing@cometdesign.com>
2025-05-13 20:49:43 -0700

    Initial upload

---
+++ SNKCR09_photos.js
@@ -0,0 +1,128 @@
+WPGgallery = new Object();
+WPGgallery.name = "Sears%20National%20Kids%20Cancer%20Ride";
+WPGgallery.photographer = "";
+WPGgallery.contact = "";
+WPGgallery.email = "";
+WPGgallery.date = "Summer, 2009"
+
+WPGgallery.colors = new Object();
+WPGgallery.colors.background = "#C0C0C0";
+WPGgallery.colors.banner = "#6699FF";
+WPGgallery.colors.text = "#000000";
+WPGgallery.colors.link = "#000000";
+WPGgallery.colors.alink = "#0000FF";
+WPGgallery.colors.vlink = "#800080";
+
+gPhotos = new Array();
+gPhotos[0] = new Object();
+gPhotos[0].filename = "SNKCR09-01.jpg";
+gPhotos[0].ImageWidth = 576;
+gPhotos[0].ImageHeight = 768;
+gPhotos[0].ThumbWidth = 93;
+gPhotos[0].ThumbHeight = 125;
+gPhotos[0].meta = new Object();
+
+gPhotos[1] = new Object();
+gPhotos[1].filename = "SNKCR09-02.jpg";
+gPhotos[1].ImageWidth = 576;
+gPhotos[1].ImageHeight = 768;
+gPhotos[1].ThumbWidth = 93;
+gPhotos[1].ThumbHeight = 125;
+gPhotos[1].meta = new Object();
+
+gPhotos[2] = new Object();
+gPhotos[2].filename = "SNKCR09-03.jpg";
+gPhotos[2].ImageWidth = 576;
+gPhotos[2].ImageHeight = 768;
+gPhotos[2].ThumbWidth = 93;
+gPhotos[2].ThumbHeight = 125;
+gPhotos[2].meta = new Object();
+
+gPhotos[3] = new Object();
+gPhotos[3].filename = "SNKCR09-04.jpg";
+gPhotos[3].ImageWidth = 576;
+gPhotos[3].ImageHeight = 768;
+gPhotos[3].ThumbWidth = 93;
+gPhotos[3].ThumbHeight = 125;
+gPhotos[3].meta = new Object();
+
+gPhotos[4] = new Object();
+gPhotos[4].filename = "SNKCR09-05.jpg";
+gPhotos[4].ImageWidth = 576;
+gPhotos[4].ImageHeight = 768;
+gPhotos[4].ThumbWidth = 93;
+gPhotos[4].ThumbHeight = 125;
+gPhotos[4].meta = new Object();
+
+gPhotos[5] = new Object();
+gPhotos[5].filename = "SNKCR09-06.jpg";
+gPhotos[5].ImageWidth = 576;
+gPhotos[5].ImageHeight = 768;
+gPhotos[5].ThumbWidth = 93;
+gPhotos[5].ThumbHeight = 125;
+gPhotos[5].meta = new Object();
+
+gPhotos[6] = new Object();
+gPhotos[6].filename = "SNKCR09-07.jpg";
+gPhotos[6].ImageWidth = 576;
+gPhotos[6].ImageHeight = 768;
+gPhotos[6].ThumbWidth = 93;
+gPhotos[6].ThumbHeight = 125;
+gPhotos[6].meta = new Object();
+
+gPhotos[7] = new Object();
+gPhotos[7].filename = "SNKCR09-08.jpg";
+gPhotos[7].ImageWidth = 576;
+gPhotos[7].ImageHeight = 768;
+gPhotos[7].ThumbWidth = 93;
+gPhotos[7].ThumbHeight = 125;
+gPhotos[7].meta = new Object();
+
+gPhotos[8] = new Object();
+gPhotos[8].filename = "SNKCR09-09.jpg";
+gPhotos[8].ImageWidth = 576;
+gPhotos[8].ImageHeight = 768;
+gPhotos[8].ThumbWidth = 93;
+gPhotos[8].ThumbHeight = 125;
+gPhotos[8].meta = new Object();
+
+gPhotos[9] = new Object();
+gPhotos[9].filename = "SNKCR09-10.jpg";
+gPhotos[9].ImageWidth = 576;
+gPhotos[9].ImageHeight = 768;
+gPhotos[9].ThumbWidth = 93;
+gPhotos[9].ThumbHeight = 125;
+gPhotos[9].meta = new Object();
+
+gPhotos[10] = new Object();
+gPhotos[10].filename = "SNKCR09-11.jpg";
+gPhotos[10].ImageWidth = 576;
+gPhotos[10].ImageHeight = 768;
+gPhotos[10].ThumbWidth = 93;
+gPhotos[10].ThumbHeight = 125;
+gPhotos[10].meta = new Object();
+
+gPhotos[11] = new Object();
+gPhotos[11].filename = "SNKCR09-12.jpg";
+gPhotos[11].ImageWidth = 576;
+gPhotos[11].ImageHeight = 768;
+gPhotos[11].ThumbWidth = 93;
+gPhotos[11].ThumbHeight = 125;
+gPhotos[11].meta = new Object();
+
+gPhotos[12] = new Object();
+gPhotos[12].filename = "SNKCR09-13.jpg";
+gPhotos[12].ImageWidth = 576;
+gPhotos[12].ImageHeight = 768;
+gPhotos[12].ThumbWidth = 93;
+gPhotos[12].ThumbHeight = 125;
+gPhotos[12].meta = new Object();
+
+gPhotos[13] = new Object();
+gPhotos[13].filename = "SNKCR09-14.jpg";
+gPhotos[13].ImageWidth = 768;
+gPhotos[13].ImageHeight = 576;
+gPhotos[13].ThumbWidth = 125;
+gPhotos[13].ThumbHeight = 93;
+gPhotos[13].meta = new Object();
+
